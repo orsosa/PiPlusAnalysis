4600337965632b6b76b208716ab995b2f164b5e8
Rodrigo Mendez <rodmendezp@gmail.com>
2015-11-13 13:49:20 -0300

    (Broadening) Finished Pt2Broad, still need testing

--- Broadening/Pt2Broad.cpp
+++ Broadening/Pt2Broad.cpp
@@ -12,6 +12,14 @@
 #include "TCut.h"
 #include "TEventList.h"
 #include "TF1.h"
+#include "TCanvas.h"
+#include "TMarker.h"
+#include "TFrame.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
 
 TString dataLoc;
 TString fDataExt;
@@ -62,12 +70,16 @@ Double_t *v_PT;
 Double_t *v_PHI;
 
 Int_t q2i, xbi, zhi;
+Int_t N_METAL = 6;
 
 TH1F *h_data_corr;
 TH1F *h_data_corr_RC;
 TFile *file;
 
 int main(int argc, char **argv){
+	Int_t met;
+	TString Metal;
+	
 	for(Int_t i = 0; i < argc; i++){
 		std::cout << "Arg " << i << std::endl;
 		std::cout << argv[i] << std::endl;
@@ -139,114 +151,56 @@ int main(int argc, char **argv){
     }
 
 	TFile *f = new TFile("pt2.root", "READ");
-
-	Double_t mean_C, mean_D_C, mean_Fe, mean_D_Fe, mean_Pb, mean_D_Pb;
-	Double_t mean_C_err, mean_D_C_err, mean_Fe_err, mean_D_Fe_err, mean_Pb_err, mean_D_Pb_err;
-	Double_t pt2b_C, pt2b_Fe, pt2b_Pb;
-	Double_t pt2b_C_err, pt2b_Fe_err, pt2b_Pb_err;
-
-	Double_t mean_C_rc1, mean_D_C_rc1, mean_Fe_rc1, mean_D_Fe_rc1, mean_Pb_rc1, mean_D_Pb_rc1;
-	Double_t mean_C_err_rc1, mean_D_C_err_rc1, mean_Fe_err_rc1, mean_D_Fe_err_rc1, mean_Pb_err_rc1, mean_D_Pb_err_rc1;
-	Double_t pt2b_C_rc1, pt2b_Fe_rc1, pt2b_Pb_rc1;
-	Double_t pt2b_C_err_rc1, pt2b_Fe_err_rc1, pt2b_Pb_err_rc1;
-
-	Double_t mean_C_rc2, mean_D_C_rc2, mean_Fe_rc2, mean_D_Fe_rc2, mean_Pb_rc2, mean_D_Pb_rc2;
-	Double_t mean_C_err_rc2, mean_D_C_err_rc2, mean_Fe_err_rc2, mean_D_Fe_err_rc2, mean_Pb_err_rc2, mean_D_Pb_err_rc2;
-	Double_t pt2b_C_rc2, pt2b_Fe_rc2, pt2b_Pb_rc2;
-	Double_t pt2b_C_err_rc2, pt2b_Fe_err_rc2, pt2b_Pb_err_rc2;
+	
+	Double_t mean[6];
+	Double_t mean_err[6];
+	Double_t pt2b[3];
+	Double_t pt2b_err[3];
+	
+	Double_t mean_rc[6];
+	Double_t mean_err_rc[6];
+	Double_t pt2b_rc[3];
+	Double_t pt2b_err_rc[3];
 
 	ofstream out;
 	out.open("Broadening.txt");
-	out << "Transverse momentum broadening vs A1/3" << endl;
-	out << "Zh\t\t\tdPt2 C\t\t\tErr C\t\t\tdPt2 Fe\t\t\tErr Fe\t\t\tdPt2 Pb\t\t\tErr Pb" << endl;
+	out << "Transverse momentum broadening vs A1/3" << std::endl;
+	out << "Zh\t\t\tdPt2 C\t\t\tErr C\t\t\tdPt2 Fe\t\t\tErr Fe\t\t\tdPt2 Pb\t\t\tErr Pb" << std::endl;
+	
+	TH1F *h;
+	TH1F *h_rc;
+	TCanvas *MyC;
+	TPad *S;
 
 	for(q2i = 0; q2i < N_Q2; q2i++){
 		for(xbi = 0; xbi < N_XB; xbi++){
 			for(zhi = 0; zhi < N_ZH; zhi++){
-				TH1F *h_C = (TH1F*) (f->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_C = h_C->GetMean();
-				mean_C_err = h_C->GetMeanError();
-				TH1F *h_C_rc1 = dynamic_cast<TH1F*>(f_C->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_C_rc1 = h_C_rc1->GetMean();
-				mean_C_err_rc1 = h_C_rc1->GetMeanError();
-				f_C_rc->cd();
-				TH1F *h_C_rc2 = dynamic_cast<TH1F*>(f_C_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_C_rc2 = h_C_rc2->GetMean();
-				mean_C_err_rc2 = h_C_rc2->GetMeanError();
-
-				// Get D_C Statistic
-				f_D_C->cd();
-				TH1F *h_D_C = dynamic_cast<TH1F*>(f_D_C->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_D_C = h_D_C->GetMean();
-				mean_D_C_err = h_D_C->GetMeanError();
-				TH1F *h_D_C_rc1 = dynamic_cast<TH1F*>(f_D_C->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_C_rc1 = h_D_C_rc1->GetMean();
-				mean_D_C_err_rc1 = h_D_C_rc1->GetMeanError();
-				f_D_C_rc->cd();
-				TH1F *h_D_C_rc2 = dynamic_cast<TH1F*>(f_D_C_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_C_rc2 = h_D_C_rc2->GetMean();
-				mean_D_C_err_rc2 = h_D_C_rc2->GetMeanError();
-
-				// Get Fe Statistic
-				f_Fe->cd();
-				TH1F *h_Fe = dynamic_cast<TH1F*>(f_Fe->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_Fe = h_Fe->GetMean();
-				mean_Fe_err = h_Fe->GetMeanError();
-				TH1F *h_Fe_rc1 = dynamic_cast<TH1F*>(f_Fe->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_Fe_rc1 = h_Fe_rc1->GetMean();
-				mean_Fe_err_rc1 = h_Fe_rc1->GetMeanError();
-				f_Fe_rc->cd();
-				TH1F *h_Fe_rc2 = dynamic_cast<TH1F*>(f_Fe_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_Fe_rc2 = h_Fe_rc2->GetMean();
-				mean_Fe_err_rc2 = h_Fe_rc2->GetMeanError();
-
-				// Get D_Fe Statistic
-				f_D_Fe->cd();
-				TH1F *h_D_Fe = dynamic_cast<TH1F*>(f_D_Fe->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_D_Fe = h_D_Fe->GetMean();
-				mean_D_Fe_err = h_D_Fe->GetMeanError();
-				TH1F *h_D_Fe_rc1 = dynamic_cast<TH1F*>(f_D_Fe->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_Fe_rc1 = h_D_Fe_rc1->GetMean();
-				mean_D_Fe_err_rc1 = h_D_Fe_rc1->GetMeanError();
-				f_D_Fe_rc->cd();
-				TH1F *h_D_Fe_rc2 = dynamic_cast<TH1F*>(f_D_Fe_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_Fe_rc2 = h_D_Fe_rc2->GetMean();
-				mean_D_Fe_err_rc2 = h_D_Fe_rc2->GetMeanError();
-
-				// Get Pb Statistic
-				f_Pb->cd();
-				TH1F *h_Pb = dynamic_cast<TH1F*>(f_Pb->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_Pb = h_Pb->GetMean();
-				mean_Pb_err = h_Pb->GetMeanError();
-				TH1F *h_Pb_rc1 = dynamic_cast<TH1F*>(f_Pb->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_Pb_rc1 = h_Pb_rc1->GetMean();
-				mean_Pb_err_rc1 = h_Pb_rc1->GetMeanError();
-				f_Pb_rc->cd();
-				TH1F *h_Pb_rc2 = dynamic_cast<TH1F*>(f_Pb_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_Pb_rc2 = h_Pb_rc2->GetMean();
-				mean_Pb_err_rc2 = h_Pb_rc2->GetMeanError();
-
-				// Get D_Pb Statistic
-				f_D_Pb->cd();
-				TH1F *h_D_Pb = dynamic_cast<TH1F*>(f_D_Pb->Get((const char*)Form("hist_%d%d%d",i1,i2,i3)));
-				mean_D_Pb = h_D_Pb->GetMean();
-				mean_D_Pb_err = h_D_Pb->GetMeanError();
-				TH1F *h_D_Pb_rc1 = dynamic_cast<TH1F*>(f_D_Pb->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_Pb_rc1 = h_D_Pb_rc1->GetMean();
-				mean_D_Pb_err_rc1 = h_D_Pb_rc1->GetMeanError();
-				f_D_Pb_rc->cd();
-				TH1F *h_D_Pb_rc2 = dynamic_cast<TH1F*>(f_D_Pb_rc->Get((const char*)Form("hist_rc_%d%d%d",i1,i2,i3)));
-				mean_D_Pb_rc2 = h_D_Pb_rc2->GetMean();
-				mean_D_Pb_err_rc2 = h_D_Pb_rc2->GetMeanError();
-
-				// Create canvas and pad for plots
-				TCanvas *MyC = new TCanvas("MyC","transverse momuntum broadening of leading pions",6,21,1117,499);
+				for(met = 0; met < N_METAL; met++){
+					if(met == 0) Metal = "C"; 
+					else if(met == 1) Metal = "Fe";
+					else if(met == 2) Metal = "Pb";
+					else if(met == 3) Metal = "DC";
+					else if(met == 4) Metal = "DFe";
+					else Metal = "DPb";
+					
+					h = (TH1F*) (f->Get((const char*)Form("%s_hist_%d%d%d", (const char*) Metal, q2i, xbi, zhi)));
+					mean[met] = h->GetMean();
+					mean_err[met] = h->GetMeanError();
+					
+					if(RCOn){
+						h_rc = (TH1F*) (f->Get((const char*)Form("%s_hist_rc_%d%d%d", (const char*) Metal, q2i, xbi, zhi)));
+						mean_rc[met] = h_rc->GetMean();
+						mean_err_rc[met] = h_rc->GetMeanError();						
+					}
+				}
+				
+				MyC = new TCanvas("MyC","transverse momuntum broadening of leading pions",6,21,1117,499);
 				MyC->SetFillColor(42);
 				MyC->SetGrid();
 				MyC->GetFrame()->SetFillColor(21);
 				MyC->GetFrame()->SetBorderSize(12);
 				MyC->Update();
-				TPad *S = new TPad("S", "transverse momuntum broadening of leading pions",0,0.160072,1,1);
+				S = new TPad("S", "transverse momuntum broadening of leading pions",0,0.160072,1,1);
 				S->Draw();
 				S->cd();
 				S->SetFillColor(42);
@@ -255,54 +209,32 @@ int main(int argc, char **argv){
 				S->GetFrame()->SetBorderSize(12);
 				S->Update();
 
-				// Calculate Broadening for RC1
-				pt2b_C_rc1 = mean_C_rc1 - mean_D_C_rc1;
-				pt2b_Fe_rc1 = mean_Fe_rc1 - mean_D_Fe_rc1;
-				pt2b_Pb_rc1 = mean_Pb_rc1 - mean_D_Pb_rc1;
-				pt2b_C_err_rc1 = TMath::Sqrt(mean_C_err_rc1*mean_C_err_rc1 + mean_D_C_err_rc1*mean_D_C_err_rc1);
-				pt2b_Fe_err_rc1 = TMath::Sqrt(mean_Fe_err_rc1*mean_Fe_err_rc1 + mean_D_Fe_err_rc1*mean_D_Fe_err_rc1);
-				pt2b_Pb_err_rc1 = TMath::Sqrt(mean_Pb_err_rc1*mean_Pb_err_rc1 + mean_D_Pb_err_rc1*mean_D_Pb_err_rc1);
-			  
-				Double_t x25_rc1[3]  = {pow(12,0.333), pow(56,0.333), pow(208,0.333)}; 
-				Double_t y25_rc1[3]  = {pt2b_C_rc1, pt2b_Fe_rc1, pt2b_Pb_rc1};
-				Double_t ex25_rc1[3] = {0, 0, 0};
-				Double_t ey25_rc1[3] = {pt2b_C_err_rc1, pt2b_Fe_err_rc1, pt2b_Pb_err_rc1}; 
-
-				TGraphErrors *gr25_rc1 = new TGraphErrors(3,x25_rc1,y25_rc1,ex25_rc1,ey25_rc1); 
-
-				// Calculate Broadening for RC2
-				pt2b_C_rc2 = mean_C_rc2 - mean_D_C_rc2;
-				pt2b_Fe_rc2 = mean_Fe_rc2 - mean_D_Fe_rc2;
-				pt2b_Pb_rc2 = mean_Pb_rc2 - mean_D_Pb_rc2;
-				pt2b_C_err_rc2 = TMath::Sqrt(mean_C_err_rc2*mean_C_err_rc2 + mean_D_C_err_rc2*mean_D_C_err_rc2);
-				pt2b_Fe_err_rc2 = TMath::Sqrt(mean_Fe_err_rc2*mean_Fe_err_rc2 + mean_D_Fe_err_rc2*mean_D_Fe_err_rc2);
-				pt2b_Pb_err_rc2 = TMath::Sqrt(mean_Pb_err_rc2*mean_Pb_err_rc2 + mean_D_Pb_err_rc2*mean_D_Pb_err_rc2);
-			  
-				Double_t x25_rc2[3]  = {pow(12,0.333), pow(56,0.333), pow(208,0.333)}; 
-				Double_t y25_rc2[3]  = {pt2b_C_rc2, pt2b_Fe_rc2, pt2b_Pb_rc2};
-				Double_t ex25_rc2[3] = {0, 0, 0};
-				Double_t ey25_rc2[3] = {pt2b_C_err_rc2, pt2b_Fe_err_rc2, pt2b_Pb_err_rc2}; 
-
-				TGraphErrors *gr25_rc2 = new TGraphErrors(3,x25_rc2,y25_rc2,ex25_rc2,ey25_rc2); 
-
-				// Calculate Broadening for no RC
-				pt2b_C = mean_C - mean_D_C;
-				pt2b_Fe = mean_Fe - mean_D_Fe;
-				pt2b_Pb = mean_Pb - mean_D_Pb;
-				pt2b_C_err = TMath::Sqrt(mean_C_err*mean_C_err + mean_D_C_err*mean_D_C_err);
-				pt2b_Fe_err = TMath::Sqrt(mean_Fe_err*mean_Fe_err + mean_D_Fe_err*mean_D_Fe_err);
-				pt2b_Pb_err = TMath::Sqrt(mean_Pb_err*mean_Pb_err + mean_D_Pb_err*mean_D_Pb_err);
-			  
+				for(met = 0; met < N_METAL/2; met++){
+					pt2b[met] = mean[met] - mean[met+3];
+					pt2b_err[met] = TMath::Sqrt(mean_err[met]*mean_err[met] + mean_err[met+3]*mean_err[met+3]);
+					if(RCOn){
+						pt2b_rc[met] = mean_rc[met] - mean_rc[met+3];
+						pt2b_err_rc[met] = TMath::Sqrt(mean_err_rc[met]*mean_err_rc[met] + mean_err_rc[met+3]*mean_err_rc[met+3]);
+					}
+				}
+							  
 				Double_t x25[3]  = {pow(12,0.333), pow(56,0.333), pow(208,0.333)}; 
-				Double_t y25[3]  = {pt2b_C, pt2b_Fe, pt2b_Pb};
+				Double_t y25[3]  = {pt2b[0], pt2b[1], pt2b[2]};
 				Double_t ex25[3] = {0, 0, 0};
-				Double_t ey25[3] = {pt2b_C_err, pt2b_Fe_err, pt2b_Pb_err}; 
+				Double_t ey25[3] = {pt2b_err[0], pt2b_err[1], pt2b_err[2]}; 
 
 				TGraphErrors *gr25 = new TGraphErrors(3,x25,y25,ex25,ey25); 
-		
+			  
+				Double_t x25_rc[3]  = {pow(12,0.333), pow(56,0.333), pow(208,0.333)}; 
+				Double_t y25_rc[3]  = {pt2b_rc[0], pt2b_rc[1], pt2b_rc[2]};
+				Double_t ex25_rc[3] = {0, 0, 0};
+				Double_t ey25_rc[3] = {pt2b_err_rc[0], pt2b_err_rc[1], pt2b_err_rc[2]}; 
+
+				TGraphErrors *gr25_rc = new TGraphErrors(3, x25_rc, y25_rc, ex25_rc, ey25_rc); 
+
 				gr25->SetMaximum(0.1);
 				gr25->SetMinimum(0);
-				TString title = Form("%.1f<Xb<%.1f %.1f<Q^{2}<%.1f %.1f<Z_{#pi^{+}}<%.1f   #void8   #pi^{+}", v_XB[i2], v_XB[i2+1], v_Q2[i1], v_Q2[i1+1], v_ZH[i3], v_ZH[i3+1]);
+				TString title = Form("%.1f<Xb<%.1f %.1f<Q^{2}<%.1f %.1f<Z_{#pi^{+}}<%.1f   #void8   #pi^{+}", v_XB[xbi], v_XB[xbi+1], v_Q2[q2i], v_Q2[q2i+1], v_ZH[zhi], v_ZH[zhi+1]);
 		
 				TH1F *Graph4 = new TH1F("Graph4", title, 100, 1.95, 6.27715);
 				Graph4->SetMinimum(0);
@@ -332,35 +264,27 @@ int main(int argc, char **argv){
 				S->Update();
 		
 				gr25->Draw("P:same");
+				
 				TMarker *m1 = new TMarker(x25[0],y25[0],21);
 				m1->SetMarkerColor(2);
 				m1->Draw("same");
-				TMarker *m1_rc1 = new TMarker(x25_rc1[0], y25_rc1[0], 21);
-				m1_rc1->SetMarkerColor(kBlue);
-				m1_rc1->Draw("same");				
-				TMarker *m1_rc2 = new TMarker(x25_rc2[0], y25_rc2[0], 21);
-				m1_rc2->SetMarkerColor(kGreen);
-				m1_rc2->Draw("same");	
+				TMarker *m1_rc = new TMarker(x25_rc[0], y25_rc[0], 21);
+				m1_rc->SetMarkerColor(kBlue);
+				m1_rc->Draw("same");				
 
 				TMarker *m2 = new TMarker(x25[1],y25[1],23);
 				m2->SetMarkerColor(2);
 				m2->Draw("same");
-				TMarker *m2_rc1 = new TMarker(x25_rc1[1], y25_rc1[1], 23);
-				m2_rc1->SetMarkerColor(kBlue);
-				m2_rc1->Draw("same");
-				TMarker *m2_rc2 = new TMarker(x25_rc2[1], y25_rc2[1], 23);
-				m2_rc2->SetMarkerColor(kGreen);
-				m2_rc2->Draw("same");
+				TMarker *m2_rc = new TMarker(x25_rc[1], y25_rc[1], 23);
+				m2_rc->SetMarkerColor(kBlue);
+				m2_rc->Draw("same");
 
 				TMarker *m3 = new TMarker(x25[2],y25[2],8);
 				m3->SetMarkerColor(2);
 				m3->Draw("same");
-				TMarker *m3_rc1 = new TMarker(x25_rc1[2], y25_rc1[2], 8);
-				m3_rc1->SetMarkerColor(kBlue);
-				m3_rc1->Draw("same");
-				TMarker *m3_rc2 = new TMarker(x25_rc2[2], y25_rc2[2], 8);
-				m3_rc2->SetMarkerColor(kGreen);
-				m3_rc2->Draw("same");
+				TMarker *m3_rc = new TMarker(x25_rc[2], y25_rc[2], 8);
+				m3_rc->SetMarkerColor(kBlue);
+				m3_rc->Draw("same");
 			
 				S->Update();
 				TGraph *gr1 = new TGraph();
@@ -382,53 +306,35 @@ int main(int argc, char **argv){
 				legend->AddEntry(gr2," Iron","p");
 				legend->AddEntry(gr3," Lead","p");
 				legend->Draw();
-				TString name=Form("source%d%d%d.cc",i1,i2,i3);
-				TString name1=Form("%d%d%d.png",i1,i2,i3);
-				TString name2=Form("%d%d%d.eps",i1,i2,i3);
-				TString name3=Form("%d%d%d.ps",i1,i2,i3);
+				TString name1=Form("%d%d%d.png", q2i, xbi, zhi);
 				MyC->cd();
-				TString subtitle=Form("C_{m}=%.4f Fe_{m}=%.4f Pb_{m}=%.4f",pt2b_C,pt2b_Fe,pt2b_Pb);
+				TString subtitle=Form("C_{m}=%.4f Fe_{m}=%.4f Pb_{m}=%.4f", pt2b[0], pt2b[1], pt2b[2]);
 				TLatex *tex = new TLatex(0.01,0.10241,subtitle);
 				tex->SetLineWidth(2);
 				tex->SetTextColor(2);
 				tex->Draw();
-				subtitle=Form("C_{e}=%.4f Fe_{e}=%.4f Pb_{e}=%.4f",pt2b_C_err,pt2b_Fe_err,pt2b_Pb_err);
+				subtitle=Form("C_{e}=%.4f Fe_{e}=%.4f Pb_{e}=%.4f", pt2b_err[0], pt2b_err[1], pt2b_err[2]);
 				tex = new TLatex(0.01,0.0281124,subtitle);
 				tex->SetLineWidth(2);
 				tex->SetTextColor(2);
 				tex->Draw();
-				subtitle = Form("C_{m}=%.4f Fe_{m}=%.4f Pb_{m}=%.4f",pt2b_C_rc1,pt2b_Fe_rc1,pt2b_Pb_rc1);
+				subtitle = Form("C_{m}=%.4f Fe_{m}=%.4f Pb_{m}=%.4f", pt2b_rc[0], pt2b_rc[1], pt2b_rc[2]);
 				tex = new TLatex(0.34, 0.10241, subtitle);
 				tex->SetLineWidth(2);
 				tex->SetTextColor(kBlue);
 				tex->Draw();
-				subtitle = Form("C_{e}=%.4f Fe_{e}=%.4f Pb_{e}=%.4f",pt2b_C_err_rc1,pt2b_Fe_err_rc1,pt2b_Pb_err_rc1);
+				subtitle = Form("C_{e}=%.4f Fe_{e}=%.4f Pb_{e}=%.4f", pt2b_err_rc[0], pt2b_err_rc[1], pt2b_err_rc[2]);
 				tex = new TLatex(0.34, 0.0281124, subtitle);
 				tex->SetLineWidth(2);
 				tex->SetTextColor(kBlue);
 				tex->Draw();
-				subtitle = Form("C_{m}=%.4f Fe_{m}=%.4f Pb_{m}=%.4f",pt2b_C_rc2,0,pt2b_Pb_rc2);
-				tex = new TLatex(0.67, 0.10241, subtitle);
-				tex->SetLineWidth(2);
-				tex->SetTextColor(kGreen);
-				tex->Draw();
-				subtitle = Form("C_{e}=%.4f Fe_{e}=%.4f Pb_{e}=%.4f",pt2b_C_err_rc2,pt2b_Fe_err_rc2,pt2b_Pb_err_rc2);
-				tex = new TLatex(0.67, 0.0281124, subtitle);
-				tex->SetLineWidth(2);
-				tex->SetTextColor(kGreen);
-				tex->Draw();
-				//TLatex *tex1 = new TLatex(0.634894,0.10241,"Corrected data");
-				//tex1->SetLineWidth(2);
-				//tex1->Draw();
 				MyC->Modified();
 				MyC->Update();
-				//MyC->SaveSource(name);
-				//MyC->SetEditable(kFALSE);
-				//MyC->SaveAs(name1);	
+				MyC->SaveAs(name1);	
 
-				out << pt2b_C_rc1 << "\t\t" << pt2b_C_err_rc1 << "\t\t" << pt2b_Fe_rc1 << "\t\t" << pt2b_Fe_err_rc1 << "\t\t" << pt2b_Pb_rc1 << "\t\t" << pt2b_Pb_err_rc1 << endl;
+				out << pt2b_rc[0] << "\t\t" << pt2b_err_rc[0] << "\t\t" << pt2b_rc[1] << "\t\t" << pt2b_err_rc[1] << "\t\t" << pt2b_rc[2] << "\t\t" << pt2b_err_rc[2] << std::endl;
 			}	
-			out << endl;
+			out << std::endl;
 		}
 	}
 	out.close();
